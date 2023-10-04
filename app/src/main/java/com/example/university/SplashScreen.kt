4f9202095b981cbdda429c18976a398768d5f47a
aldhi saputra <aldhisaputra795@gmail.com>
2023-10-04 23:31:19 +0700

    SplashScreen fix

--- app/src/main/java/com/example/university/SplashScreen.kt
+++ app/src/main/java/com/example/university/SplashScreen.kt
@@ -1,13 +1,23 @@
 package com.example.university
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        },5000)
     }
+
 }
+
