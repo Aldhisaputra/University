ffab83702708c3efa7681f043df2dba492e67869
aldhi saputra <aldhisaputra795@gmail.com>
2023-10-08 23:27:57 +0700

    profile activity

--- app/src/main/java/com/example/university/MainActivity.kt
+++ app/src/main/java/com/example/university/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.university
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_profile, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.profilku -> {
+                val profilku = Intent(this@MainActivity, ProfileActivity::class.java)
+                startActivity(profilku)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/university/ProfileActivity.kt
@@ -0,0 +1,15 @@
+package com.example.university
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.hide()
+
+    }
+}
