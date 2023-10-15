2088d0ca20327613dbad4a13d37bf332297610e3
aldhi saputra <aldhisaputra795@gmail.com>
2023-10-15 11:37:41 +0700

    menambahkan ListUniversityAdapter

---
+++ app/src/main/java/com/example/university/ListUniversityAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.university
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListUniversityAdapter(private val listUniversity: ArrayList<University>) :RecyclerView.Adapter<ListUniversityAdapter.ListViewHolder>() {
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
