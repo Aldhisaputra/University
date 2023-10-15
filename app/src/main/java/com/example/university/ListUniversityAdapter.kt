8f78125202a54d914d0bb244e680014dd8e15f06
aldhi saputra <aldhisaputra795@gmail.com>
2023-10-15 20:40:15 +0700

    menambahkan kodingan fix ListUniversityAdapter & MainActivity

--- app/src/main/java/com/example/university/ListUniversityAdapter.kt
+++ app/src/main/java/com/example/university/ListUniversityAdapter.kt
@@ -1,23 +1,30 @@
 package com.example.university
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ListUniversityAdapter(private val listUniversity: ArrayList<University>) :RecyclerView.Adapter<ListUniversityAdapter.ListViewHolder>() {
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_university, parent, false)
+        return ListViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = listUniversity.size
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val (name, description, photo) = listUniversity[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
     }
 }
--- app/src/main/java/com/example/university/MainActivity.kt
+++ app/src/main/java/com/example/university/MainActivity.kt
@@ -5,11 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvUniversity: RecyclerView
+    private val list = ArrayList<University>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvUniversity = findViewById(R.id.rv_University)
+        rvUniversity.setHasFixedSize(true)
+
+        list.addAll(getListUniversity())
+        showRecyclerList()
+    }
+
+    private fun getListUniversity(): ArrayList<University> {
+        val dataName = resources.getStringArray(R.array.name_univerity)
+        val dataDescription = resources.getStringArray(R.array.description_university)
+        val dataPhoto = resources.obtainTypedArray(R.array.photo_university)
+        val listHero = ArrayList<University>()
+        for (i in dataName.indices) {
+            val hero = University(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+            listHero.add(hero)
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        rvUniversity.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListUniversityAdapter(list)
+        rvUniversity.adapter = listHeroAdapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
